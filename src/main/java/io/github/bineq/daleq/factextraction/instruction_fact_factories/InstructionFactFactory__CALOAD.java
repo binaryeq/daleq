a3244e6b254034081e5b1b3b71bc92aedbdbfb64
Jens Dietrich <jens.dietrich@gmail.com>
2025-01-09 17:32:05 +1300

    added more predicates

---
+++ src/main/java/io/github/bineq/daleq/factextraction/instruction_fact_factories/InstructionFactFactory__CALOAD.java
@@ -0,0 +1,22 @@
+package io.github.bineq.daleq.factextraction.instruction_fact_factories;
+
+import javax.annotation.processing.Generated;
+import io.github.bineq.daleq.factextraction.Fact;
+
+@Generated(value="io.github.bineq.daleq.factextraction.InstructionFactFactoryCodeGenerator", date= "2025-01-09T04:30Z",
+      comments= "factory generated from ASM tree API nodes")
+public class InstructionFactFactory__CALOAD implements io.github.bineq.daleq.factextraction.InstructionPredicateFactFactory<org.objectweb.asm.tree.InsnNode> {
+
+    @Override public String getVersion() {
+        return "10fd1b7e-cde5-413d-864a-44a616f37668";
+    }
+
+    @Override public io.github.bineq.daleq.factextraction.InstructionPredicate getPredicate() {
+        return io.github.bineq.daleq.factextraction.FactExtractor.REGISTRY.get(52);
+    }
+
+    @Override public Fact createFact(org.objectweb.asm.tree.InsnNode node,String methodRef,int instructionCounter) {
+        return new io.github.bineq.daleq.factextraction.SimpleFact(io.github.bineq.daleq.factextraction.FactExtractor.REGISTRY.get(52),new Object[]{methodRef,instructionCounter});
+    }
+
+};
