0fc50e153a7958cca62b3ed2c06f35bd158f634b
Jens Dietrich <jens.dietrich@gmail.com>
2025-01-10 11:28:02 +1300

    added support for more predicates / instructions, added class to infer more predicates

---
+++ src/main/java/io/github/bineq/daleq/factextraction/instruction_fact_factories/InstructionFactFactory__LCONST_0.java
@@ -0,0 +1,22 @@
+package io.github.bineq.daleq.factextraction.instruction_fact_factories;
+
+import javax.annotation.processing.Generated;
+import io.github.bineq.daleq.factextraction.Fact;
+
+@Generated(value="io.github.bineq.daleq.factextraction.InstructionFactFactoryCodeGenerator", date= "2025-01-09T22:22Z",
+      comments= "factory generated from ASM tree API nodes")
+public class InstructionFactFactory__LCONST_0 implements io.github.bineq.daleq.factextraction.InstructionPredicateFactFactory<org.objectweb.asm.tree.InsnNode> {
+
+    @Override public String getVersion() {
+        return "9eb162ad-6191-4407-b651-ebec8bbc3662";
+    }
+
+    @Override public io.github.bineq.daleq.factextraction.InstructionPredicate getPredicate() {
+        return io.github.bineq.daleq.factextraction.FactExtractor.REGISTRY.get(9);
+    }
+
+    @Override public Fact createFact(org.objectweb.asm.tree.InsnNode node,String methodRef,int instructionCounter) {
+        return new io.github.bineq.daleq.factextraction.SimpleFact(io.github.bineq.daleq.factextraction.FactExtractor.REGISTRY.get(9),new Object[]{methodRef,instructionCounter});
+    }
+
+};
---
+++ src/main/java/io/github/bineq/daleq/misc/Foo.java
@@ -0,0 +1,14 @@
+package io.github.bineq.daleq.misc;
+
+
+/**
+ * The purpose of this class is to generate byte code instructions
+ * to trigger code generation / predicate inference.
+ * @author jens dietrich
+ */
+public class Foo {
+    private long LONG_0 = 0;  // LCONST_0
+    private long LONG_1 = 1;  // LCONST_1
+    private long LONG_OTHER = 2;  //
+
+}
