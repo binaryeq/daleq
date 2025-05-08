a52a232a5a774159f4de8e00cba660620412a798
Jens Dietrich <jens.dietrich@gmail.com>
2025-05-08 12:43:57 +1200

    fix issue 18

--- src/main/java/io/github/bineq/daleq/edb/instruction_fact_factories/InstructionFactFactory__TABLESWITCH.java
+++ src/main/java/io/github/bineq/daleq/edb/instruction_fact_factories/InstructionFactFactory__TABLESWITCH.java
@@ -1,7 +1,9 @@
 package io.github.bineq.daleq.edb.instruction_fact_factories;
 
 import javax.annotation.processing.Generated;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import io.github.bineq.daleq.SimpleFact;
 import io.github.bineq.daleq.edb.EBDInstructionPredicate;
@@ -10,8 +12,10 @@ import org.objectweb.asm.tree.LabelNode;
 import io.github.bineq.daleq.Fact;
 import org.objectweb.asm.tree.TableSwitchInsnNode;
 
-@Generated(value="io.github.bineq.daleq.factextraction.InstructionFactFactoryCodeGenerator", date= "2025-02-13T10:40Z",
-      comments= "factory generated from ASM tree API nodes")
+@Generated(value="io.github.bineq.daleq.factextraction.InstructionFactFactoryCodeGenerator", date= "2025-02-13T10:40Z", comments= "factory generated from ASM tree API nodes")
+
+// WARNING: manually added code to serialise labels !
+
 public class InstructionFactFactory__TABLESWITCH implements io.github.bineq.daleq.edb.InstructionPredicateFactFactory<org.objectweb.asm.tree.TableSwitchInsnNode> {
 
     @Override public String getVersion() {
@@ -23,7 +27,15 @@ public class InstructionFactFactory__TABLESWITCH implements io.github.bineq.dale
     }
 
     @Override public Fact createFact(String factId, TableSwitchInsnNode node, String methodRef, int instructionCounter, Map<LabelNode, String> labelMap) {
-        return new SimpleFact(EDBPredicateRegistry.INSTRUCTION_PREDICATES.get(170),new Object[]{factId,methodRef,instructionCounter,labelMap.get(node.dflt),node.labels,node.max,node.min});
+        return new SimpleFact(EDBPredicateRegistry.INSTRUCTION_PREDICATES.get(170),new Object[]{factId,methodRef,instructionCounter,labelMap.get(node.dflt),serializeLabels(labelMap,node.labels),node.max,node.min});
     }
+
+    // manually inserted code !
+    private String serializeLabels(Map<LabelNode, String> labelMap, List<LabelNode> labelNodes) {
+        return labelNodes.stream()
+            .map(node -> labelMap.get(node))
+            .collect(Collectors.joining(","));
+    }
+
 
 };
--- src/test/java/io/github/bineq/daleq/edb/Issue18SecondTest.java
+++ src/test/java/io/github/bineq/daleq/edb/Issue18SecondTest.java
@@ -2,25 +2,29 @@ package io.github.bineq.daleq.edb;
 
 import io.github.bineq.daleq.Fact;
 import org.junit.jupiter.api.Test;
-
 import java.util.List;
 import java.util.stream.Collectors;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-public class Issue18FirstTest extends AbstractFactExtractionTest {
+public class Issue18SecondTest extends AbstractFactExtractionTest {
     @Override
     protected String getTestClass() {
-        return "/issue18/ReaderConfig.class";
+        return "/issue18/StreamBootstrapper.class";
     }
 
 
     @Test
     public void test() {
-        String methodRef = FactExtractor.getMethodReference("com/ctc/wstx/api/ReaderConfig", "setProperty","(Ljava/lang/String;ILjava/lang/Object;)Z");
+        String methodRef = FactExtractor.getMethodReference("com/ctc/wstx/io/StreamBootstrapper", "resolveStreamEncoding","()V");
         List<Fact> facts = getInstructionFacts(methodRef).stream()
-            .filter(fact -> fact.predicate().getName().equals("IDB_TABLESWITCH"))
+            .filter(fact -> fact.predicate().getName().equals("LOOKUPSWITCH"))
             .collect(Collectors.toUnmodifiableList());
+        assertTrue(facts.size()>0);
+
+        String labelsTerm = facts.get(0).values()[5].toString();
+        assertFalse(labelsTerm.contains("org.objectweb.asm.tree.LabelNode@"));
+
 
     }
 }
