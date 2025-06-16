335e446a6218ec7eba863763318386a38038325c
Jens Dietrich <jens.dietrich@gmail.com>
2025-06-16 17:22:58 +1200

    if a resource is not found in one of the jars, a report is generated

--- src/main/java/io/github/bineq/daleq/cli/ResourceIsPresentAnalyser.java
+++ src/main/java/io/github/bineq/daleq/cli/ResourceIsPresentAnalyser.java
@@ -1,8 +1,13 @@
 package io.github.bineq.daleq.cli;
 
 import io.github.bineq.daleq.IOUtil;
+
 import java.io.IOException;
+import java.net.URL;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 /**
@@ -10,22 +15,33 @@ import java.util.Set;
  * @author jens dietrich
  */
 public class ResourceIsPresentAnalyser implements Analyser {
+
+    private static final URL DIFF_TEMPLATE = DaleqAnalyser.class.getResource("/cli/io.github.bineq.daleq.cli.ResourceIsPresentAnalyser/diff.html");
+
+
     @Override
     public AnalysisResult analyse(String resource, Path jar1, Path jar2,Path contextDir) throws IOException {
+        List<AnalysisResultAttachment> attachments = new ArrayList<>();
         Set<String> resources1 = IOUtil.nonDirEntries(jar1);
         Set<String> resources2 = IOUtil.nonDirEntries(jar2);
         if (resources1.contains(resource) && resources2.contains(resource)) {
-            return new AnalysisResult(AnalysisResultState.PASS,"");
+            return new AnalysisResult(AnalysisResultState.PASS,"",attachments);
         }
         else if (resources1.contains(resource) && !resources2.contains(resource)) {
-            return new AnalysisResult(AnalysisResultState.FAIL,"resource missing in " + jar1.toString());
+            Map<String,String> bindings = Map.of("resource",resource,"jar_present",jar1.toString(),"jar_missing",jar2.toString());
+            String link = ResourceUtil.createReportFromTemplate(contextDir,this, resource, DIFF_TEMPLATE,"diff.html", bindings);
+            attachments.add(new AnalysisResultAttachment("diff",link,AnalysisResultAttachment.Kind.DIFF));
+            return new AnalysisResult(AnalysisResultState.FAIL,"resource missing in " + jar1.toString(),attachments);
         }
         else if (resources2.contains(resource) && !resources1.contains(resource)) {
-            return new AnalysisResult(AnalysisResultState.FAIL,"resource missing in " + jar2.toString());
+            Map<String,String> bindings = Map.of("resource",resource,"jar_present",jar2.toString(),"jar_missing",jar1.toString());
+            String link = ResourceUtil.createReportFromTemplate(contextDir,this, resource, DIFF_TEMPLATE,"diff.html", bindings);
+            attachments.add(new AnalysisResultAttachment("diff",link,AnalysisResultAttachment.Kind.DIFF));
+            return new AnalysisResult(AnalysisResultState.FAIL,"resource missing in " + jar2.toString(),attachments);
         }
         else {
             assert false : "resource missing in both jars";
-            return new AnalysisResult(AnalysisResultState.ERROR,"resource missing in both jars");
+            return new AnalysisResult(AnalysisResultState.ERROR,"resource missing in both jars",attachments);
         }
     }
 
