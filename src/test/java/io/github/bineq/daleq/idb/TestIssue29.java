8d112eddfd5ee2a89e6b05d0ef131eb70b8c6a00
Jens Dietrich <jens.dietrich@gmail.com>
2025-06-25 09:46:05 +1200

    added guards to null check rules to ensure soundness (stack stays the same), updates issue #29

--- src/test/java/io/github/bineq/daleq/idb/TestIssue29.java
+++ src/test/java/io/github/bineq/daleq/idb/TestIssue29.java
@@ -1,17 +1,13 @@
 package io.github.bineq.daleq.idb;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import java.util.List;
 import java.util.function.Predicate;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-// test for support for bytecode changes in issue19
-
-// see issue 29 for discussion
-
-@Disabled public class TestIssue29 extends AbstractIDBTest {
+// test for support for bytecode changes in issue29
+public class TestIssue29 extends AbstractIDBTest {
 
     @Override
     public String getRulesPath() {
@@ -20,7 +16,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
     @Override
     public String getPathOfClassUnderTest() {
-        return "/issue29/jar1/AbstractYAMLBasedConfiguration.class";
+        return "/issue29/jar2/AbstractYAMLBasedConfiguration.class";
     }
 
     public static final String METHOD_ID = "org/apache/commons/configuration2/AbstractYAMLBasedConfiguration::lambda$parseMap$1(Lorg/apache/commons/configuration2/tree/ImmutableNode$Builder;Ljava/lang/String;Ljava/lang/Object;)V";
@@ -35,12 +31,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
         Predicate<String[]> methodContext = line -> line[methodIdSlotPosition].equals(METHOD_ID);
         Predicate<String[]> firstInstructionId = line -> line[instructionCounterSlotPosition].equals(LINE);
 
-        List<String[]> facts = this.getEDBFacts("INVOKESTATIC",methodContext.and(firstInstructionId));
+        List<String[]> facts = this.getEDBFacts("INVOKEVIRTUAL",methodContext.and(firstInstructionId));
         assertEquals(1,facts.size());
         String[] fact = facts.get(0);
-        assertEquals("java/util/Objects",fact[3]);
-        assertEquals("requireNonNull",fact[4]);
-//        assertEquals("(Ljava/lang/Object;)Ljava/lang/Object;",fact[5]);
+
+        assertEquals("java/lang/Object",fact[3]);
+        assertEquals("getClass",fact[4]);
+        assertEquals("()Ljava/lang/Class;",fact[5]);
 
     }
 
@@ -53,12 +50,12 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
         Predicate<String[]> methodContext = line -> line[methodIdSlotPosition].equals(METHOD_ID);
         Predicate<String[]> firstInstructionId = line -> line[instructionCounterSlotPosition].equals(LINE);
 
-        List<String[]> facts = this.getIDBFacts("IDB_INVOKEVIRTUAL",methodContext.and(firstInstructionId));
+        List<String[]> facts = this.getIDBFacts("IDB_INVOKESTATIC",methodContext.and(firstInstructionId));
         assertEquals(1,facts.size());
         String[] fact = facts.get(0);
-        assertEquals("java/lang/Object",fact[3]);
-        assertEquals("getClass",fact[4]);
-        assertEquals("()Ljava/lang/Class;",fact[5]);
+        assertEquals("java/util/Objects",fact[3]);
+        assertEquals("requireNonNull",fact[4]);
+        assertEquals("(Ljava/lang/Object;)Ljava/lang/Object;",fact[5]);
 
     }
 
