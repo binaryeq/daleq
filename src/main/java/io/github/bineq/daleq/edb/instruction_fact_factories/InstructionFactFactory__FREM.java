49d62ea0308c2e8d2179f2eff03182e36a0a776b
Jens Dietrich <jens.dietrich@gmail.com>
2025-07-19 12:02:17 +1200

    added support for FREM bytecode instruction

---
+++ src/main/java/io/github/bineq/daleq/edb/instruction_fact_factories/InstructionFactFactory__FREM.java
@@ -0,0 +1,22 @@
+package io.github.bineq.daleq.edb.instruction_fact_factories;
+
+import javax.annotation.processing.Generated;
+import io.github.bineq.daleq.Fact;
+
+@Generated(value="io.github.bineq.daleq.edb.InstructionFactFactoryCodeGenerator", date= "2025-07-19T00:01Z",
+      comments= "factory generated from ASM tree API nodes")
+public class InstructionFactFactory__FREM implements io.github.bineq.daleq.edb.InstructionPredicateFactFactory<org.objectweb.asm.tree.InsnNode> {
+
+    @Override public String getVersion() {
+        return "02f5e92e-38d7-40e2-9177-4f70c1b31752";
+    }
+
+    @Override public io.github.bineq.daleq.edb.EDBInstructionPredicate getPredicate() {
+        return io.github.bineq.daleq.edb.EDBPredicateRegistry.INSTRUCTION_PREDICATES.get(114);
+    }
+
+    @Override public Fact createFact(String factid,org.objectweb.asm.tree.InsnNode node,String methodRef,int instructionCounter,io.github.bineq.daleq.edb.FactExtractor.LabelMap labelMap) {
+        return new io.github.bineq.daleq.SimpleFact(io.github.bineq.daleq.edb.EDBPredicateRegistry.INSTRUCTION_PREDICATES.get(114),new Object[]{factid,methodRef,instructionCounter});
+    }
+
+};
